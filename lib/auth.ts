847c3eed2bb1c74261442694173777793742f97a
Vansh Sharma <sharmavansh.3777@gmail.com>
2025-12-30 12:57:24 +0530

    changed auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth'
 import GoogleProvider from "next-auth/providers/google";
@@ -10,6 +11,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID || "",
       clientSecret: process.env.GOOGLE_SECRET || "",
+      allowDangerousEmailAccountLinking: true
     })
   ],
   secret: process.env.AUTH_SECRET,
@@ -17,26 +19,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   session: {
     strategy: "database", 
   },
-  
   callbacks: {
     async session({ session, user }) {
+   
       if (user) {
         session.user.id = user.id;
       }
       return session;
-    },
-  },
-
-  // Simplified cookie config that works in production
-  cookies: {
-    sessionToken: {
-      name: `${process.env.NODE_ENV === 'production' ? '__Secure-' : ''}next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: process.env.NODE_ENV === 'production',
-      },
     },
   },
 
