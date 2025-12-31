9e089714ee38bda5998cee81c4045787be7843c9
Vansh Sharma <sharmavansh.3777@gmail.com>
2025-12-31 19:13:15 +0530

    removed manifest.json , sw.js and providers.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,44 @@
-import "./globals.css"
-import Providers from "./providers"
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "sonner"
+import { Metadata } from "next";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "LinkIT",
+  description: "Save links. Remember why.",
+  icons:{
+    icon:'/logo.png'
+  } 
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+    return (
+    <SessionProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+           {children}
+          <Toaster richColors position="bottom-center" />
+        </body>
+      </html>
+    </SessionProvider>
+  );
 }
